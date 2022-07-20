5d4e02617488fe44f68b27eec8b47c466897d480
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2022-07-20 10:10:29 +0430

    receiptPrinterTP07ErrorArrON is added

--- js/receiptPrinterTP07.js
+++ js/receiptPrinterTP07.js
@@ -12,3 +12,28 @@ ledOn.addEventListener('change', function (e) {
 ledOff.addEventListener('change', function (e) {
   console.log(e.target.checked);
 });
+
+const receiptPrinterTP07ErrorArrON = [
+  {
+    1: 'The automatic cutter does not work because it does not leave the home position or does not reach the home position',
+  },
+  { 2: "The carrier will not leave it's home position." },
+  {
+    3: 'A paper jam is detected between print starting position and TOF Sensors',
+  },
+  { 4: "The printer doesn't find the control mark" },
+  {
+    5: 'A paper jam is detected after the printer cuts the paper. This occurs in the printers presenter.',
+  },
+  {
+    6: "A paper jam is detected after the printer cuts the paper. This occurs in the printer's retractor.",
+  },
+  { 7: "After cutting, the printer doesn't find TOF." },
+  {
+    8: 'Paper is after power on not at the print starting position. This error is enabled by memory switch 7-4.',
+  },
+  {
+    '...':
+      'StartOfJob timeout occurred because no EndOfJob was received within the specified time.',
+  },
+];
