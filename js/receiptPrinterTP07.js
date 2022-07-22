581548ee8f8c3684d5435ef84a6e7ae3c6c207be
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2022-07-22 21:50:36 +0430

    add function for LED on Error

--- js/receiptPrinterTP07.js
+++ js/receiptPrinterTP07.js
@@ -39,7 +39,12 @@ const receiptPrinterTP07ErrorArrOFF = [
 ];
 
 ledOn.addEventListener('change', function (e) {
-  console.log(e.target.checked);
+  e.target.checked &&
+    receiptPrinterTP07ErrorArrON.forEach(item => {
+      const option = document.createElement('option');
+      option.textContent = `${Object.keys(item)}`;
+      receiptPrinterTP07.appendChild(option);
+    });
 });
 ledOff.addEventListener('change', function (e) {
   console.log(e.target.checked);
