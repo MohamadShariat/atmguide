83e7f503362ca52df2ad8730e9bd0a00f12e2d31
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2022-08-11 20:38:58 +0430

    version 001 TP06 is compeleted

--- js/journalPrinterTP06.js
+++ js/journalPrinterTP06.js
@@ -1,12 +1,13 @@
 const ledOn = document.querySelector('#on');
 const ledOff = document.querySelector('#off');
 
-const receiptPrinterTP06 = document.querySelector('#journalPrinterTP06');
-const receiptPrinterTP06ErrorDes = document.querySelector(
+const journalPrinterTP06 = document.querySelector('#journalPrinterTP06');
+const journalPrinterTP06ErrorDes = document.querySelector(
   '#journalPrinterTP06-error-des'
 );
 
-const receiptPrinterTP06ErrorArrON = [
+const journalPrinterTP06ErrorArrON = [
+  { 'Please select...': '' },
   {
     7: "The printer doesn't find TOF.",
   },
@@ -20,7 +21,8 @@ const receiptPrinterTP06ErrorArrON = [
   },
 ];
 
-const receiptPrinterTP07ErrorArrOFF = [
+const journalPrinterTP06ErrorArrOFF = [
+  { 'Please select...': '' },
   { 1: 'There is an abnormally of the print head temperature.' },
   { 3: 'The power supply voltage is extremely high.' },
   { 4: 'The power supply voltage is extremely low.' },
@@ -28,8 +30,88 @@ const receiptPrinterTP07ErrorArrOFF = [
 ];
 
 ledOn.addEventListener('change', function (e) {
-  console.log(e.target.checked);
+  journalPrinterTP06.textContent = '';
+  journalPrinterTP06ErrorDes.textContent = '';
+
+  if (ledOn.checked) {
+    journalPrinterTP06ErrorArrON.forEach(item => {
+      const option = document.createElement('option');
+      option.textContent = `${Object.keys(item)}`;
+      journalPrinterTP06.appendChild(option);
+    });
+
+    journalPrinterTP06.addEventListener('change', e => {
+      e.preventDefault();
+
+      switch (e.target.value) {
+        case 'Please select...':
+          journalPrinterTP06ErrorDes.textContent = Object.values(
+            journalPrinterTP06ErrorArrON[0]
+          );
+          break;
+        case '7':
+          journalPrinterTP06ErrorDes.textContent = Object.values(
+            journalPrinterTP06ErrorArrON[1]
+          );
+          break;
+        case '8':
+          journalPrinterTP06ErrorDes.textContent = Object.values(
+            journalPrinterTP06ErrorArrON[2]
+          );
+          break;
+        case '9':
+          journalPrinterTP06ErrorDes.textContent = Object.values(
+            journalPrinterTP06ErrorArrON[3]
+          );
+          break;
+
+        case '...':
+          journalPrinterTP06ErrorDes.textContent = Object.values(
+            journalPrinterTP06ErrorArrON[4]
+          );
+          break;
+      }
+    });
+  }
 });
 ledOff.addEventListener('change', function (e) {
-  console.log(e.target.checked);
+  journalPrinterTP06.textContent = '';
+  journalPrinterTP06ErrorDes.textContent = '';
+
+  journalPrinterTP06ErrorArrOFF.forEach(item => {
+    const option = document.createElement('option');
+    option.textContent = `${Object.keys(item)}`;
+    journalPrinterTP06.appendChild(option);
+  });
+
+  journalPrinterTP06.addEventListener('change', e => {
+    e.preventDefault();
+    switch (e.target.value) {
+      case 'Please select...':
+        journalPrinterTP06ErrorDes.textContent = Object.values(
+          journalPrinterTP06ErrorArrOFF[0]
+        );
+        break;
+      case '1':
+        journalPrinterTP06ErrorDes.textContent = Object.values(
+          journalPrinterTP06ErrorArrOFF[1]
+        );
+        break;
+      case '3':
+        journalPrinterTP06ErrorDes.textContent = Object.values(
+          journalPrinterTP06ErrorArrOFF[2]
+        );
+        break;
+      case '4':
+        journalPrinterTP06ErrorDes.textContent = Object.values(
+          journalPrinterTP06ErrorArrOFF[3]
+        );
+        break;
+      case '5':
+        journalPrinterTP06ErrorDes.textContent = Object.values(
+          journalPrinterTP06ErrorArrOFF[4]
+        );
+        break;
+    }
+  });
 });
